a16ecf229a3e784e7b22b756cbc9950de98b54ea
Andres Castro Mamani <castroandresrm@gmail.com>
2023-10-09 18:02:53 -0400

    Feat: Media player design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+    component: HomePageComponent,
+    loadChildren: () => import('@modules/home/home.module').then(m => m.HomeModule)
   }
 ];
 
--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit {
-
+  mockCover: any = {
+    cover: 'https://images.freeimages.com/images/large-previews/f87/halloween-1467758.jpg',
+    name: 'Arriba',
+    album: '1000 Canciones'
+  }
   constructor() { }
 
   ngOnInit(): void {
